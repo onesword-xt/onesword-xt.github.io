1850ba3c4945c03af49860e499567849bb6eb049
dada-06 <2719669919@qq.com>
2026-02-04 02:29:05 +0800

    Site updated: 2026-02-04 02:29:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/53dec9d0.html","posts/knight2026.html"];function toRandomPost(){
+var posts=["posts/53dec9d0.html","posts/knight2026.html","posts/UniCTF2026.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
